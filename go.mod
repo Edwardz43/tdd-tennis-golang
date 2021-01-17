42b0f5fdebf71518497b9ac66080172591731f38
EdLo <b93303015@gmail.com>
2021-01-17 10:05:08 +0800

    add first test case and force it pass

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module Edwardz43/tennis
 
 go 1.15
+
+require (
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869
+	github.com/kr/pretty v0.2.1 // indirect
+)
--- tennis_test.go
+++ tennis_test.go
@@ -1,1 +1,11 @@
 package tennis_test
+
+import (
+	"testing"
+
+	"github.com/bmizerany/assert"
+)
+
+func TestShouldBeLoveAll(t *testing.T) {
+	assert.Equal(t, "love all", "love all")
+}
