088ce314708d15647b43cc0602db89dd1221aee0
EdLo <b93303015@gmail.com>
2021-01-17 10:34:52 +0800

    move tennis struct to global when test init

--- tennis_test.go
+++ tennis_test.go
@@ -7,25 +7,28 @@ import (
 	"github.com/bmizerany/assert"
 )
 
+var tenn *tennis.Tennis
+
+func init() {
+	tenn = new(tennis.Tennis)
+}
+
+func addFirstPlayerScore() {
+	for i := 0; i < 2; i++ {
+		tenn.FirstPlayerScore()
+	}
+}
+
 func TestShouldBeLoveAll(t *testing.T) {
-	tenn := new(tennis.Tennis)
 	assert.Equal(t, "love all", tenn.Score())
 }
 
 func TestShouldBeFifteenLove(t *testing.T) {
-	tenn := new(tennis.Tennis)
 	tenn.FirstPlayerScore()
 	assert.Equal(t, "fifteen love", tenn.Score())
 }
 
 func TestShouldBeThirtyLove(t *testing.T) {
-	tenn := new(tennis.Tennis)
-	addFirstPlayerScore(tenn)
+	addFirstPlayerScore()
 	assert.Equal(t, "thirty love", tenn.Score())
-}
-
-func addFirstPlayerScore(tenn *tennis.Tennis) {
-	for i := 0; i < 2; i++ {
-		tenn.FirstPlayerScore()
-	}
 }
